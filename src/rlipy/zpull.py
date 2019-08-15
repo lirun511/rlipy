d9640598c0cc96e9c15fb0b4cfdb413ebad53636
Ron Li <rli@vrnetf04.veritionfund.com>
2019-08-15 08:46:47 -0400

    remove encode/decode in zpull

--- src/rlipy/zpull.py
+++ src/rlipy/zpull.py
@@ -1,8 +1,12 @@
-import zmq
 import argparse
-import time
 import sys
+import time
+
+import zmq
+
+
 class zpull(object):
+
     def __init__(self, context, address):
         self.context = context
         self.socket = self.context.socket(zmq.PULL)
@@ -12,43 +16,43 @@ class zpull(object):
 
         self.address = address
         self.socket.bind(address)
-    
+
     def recv(self):
         try:
-            msg = self.socket.recv().decode('ascii')
+            msg = self.socket.recv()
             return msg
         except zmq.Again as e:
             return None
-        
+
     def close(self):
         self.socket.close()
-        
+
     def __del__(self):
         self.close()
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument(dest="address", type=str, help='zmq push address')
-    parser.add_argument('-f', dest="file", type=str, help='input file')
+    parser.add_argument(dest = "address", type = str, help = 'zmq push address')
+    parser.add_argument('-f', dest = "file", type = str, help = 'input file')
 
     args = parser.parse_args()
     zcontext = zmq.Context()
-    socket = zpull(zcontext, "tcp://"+args.address)
+    socket = zpull(zcontext, "tcp://" + args.address)
     zpoller = zmq.Poller()
     zpoller.register(socket.socket, zmq.POLLIN)
     while(True):
         try:
           events = dict(zpoller.poll(1000))
-          if(socket.socket in events and events[socket.socket]==zmq.POLLIN):
+          if(socket.socket in events and events[socket.socket] == zmq.POLLIN):
              msg = socket.socket.recv()
-             print( msg )
+             print(msg)
              sys.stdout.flush()
         except KeyboardInterrupt:
             break
         except:
             break
 
-    socket.close()   
+    socket.close()
     zcontext.term()
 
-    
